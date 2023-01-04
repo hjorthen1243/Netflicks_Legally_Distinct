dca86086d52511e489f4dd9db827cbad19e00d45
TheDemon12345678910 <anehoimark@gmail.com>
2023-01-04 11:52:37 +0100

    DeleteMovie

--- src/GUI/Controller/DeleteMovieController.java
+++ src/GUI/Controller/DeleteMovieController.java
@@ -1,6 +1,16 @@
 package GUI.Controller;
 
+import GUI.Model.Model;
+import javafx.event.ActionEvent;
+
 public class DeleteMovieController {
 
+    public void ClickedYes(ActionEvent event) {
+        System.out.println("hej");
+    }
 
+
+    public void ClickedNo(ActionEvent event) {
+        System.out.println("hej igen");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import GUI.Controller.DeleteMovieController;
+import GUI.Model.Model;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-        
-        System.out.println("Hello world!");
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/View/DeleteMovie.fxml"));
+        Scene scene = new Scene(root); primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Del");
+        primaryStage.show();
     }
 }
