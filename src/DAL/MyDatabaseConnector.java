0707d8ef94300cd63750f096961f8d7ce8c0ea3c
TheDemon12345678910 <anehoimark@gmail.com>
2023-01-05 09:10:38 +0100

    MyDatabaseConnector

---
+++ src/DAL/MyDatabaseConnector.java
@@ -0,0 +1,26 @@
+package DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+
+public class MyDatabaseConnector {
+    private SQLServerDataSource dataSource; //variable for creating a connection to the DB
+
+    public MyDatabaseConnector() {
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("MyTunesGRP32022");
+        dataSource.setUser("CSe22A_27");
+        dataSource.setPassword("CSe22A_27");
+        dataSource.setTrustServerCertificate(true);
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        System.out.println("Connection to DB is successful");
+        return dataSource.getConnection();
+    }
+}
+
+
