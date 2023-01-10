5c43a29421be5ffd8298ba21aee6c5d5cbe55d21
TheDemon12345678910 <anehoimark@gmail.com>
2023-01-10 12:37:03 +0100

    PutInnAllCategories

--- src/BLL/CategoryManager.java
+++ src/BLL/CategoryManager.java
@@ -1,9 +1,22 @@
 package BLL;
 
 import BE.Category;
+import DAL.CategoryDAO;
+import GUI.Model.CategoryModel;
+
+import java.util.ArrayList;
 
 public class CategoryManager {
+    CategoryDAO categoryDAO;
+
     public Category getAllCategories() {
+        categoryDAO = new CategoryDAO();
+        try {
+            //ArrayList<Category> categories = categoryDAO.getAllCategories();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
