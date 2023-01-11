efa0643680024b9037e59e253194b40554199945
hjorthen1243 <114651351+hjorthen1243@users.noreply.github.com>
2023-01-11 09:22:17 +0100

    EditView + EditViewController added

---
+++ src/GUI/Controller/EditViewController.java
@@ -0,0 +1,20 @@
+package GUI.Controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class EditViewController {
+    public DatePicker datePickerLastSeen;
+    public TextField txtFieldPRating;
+    public TextField txtFieldAddCatDb;
+    public Button btnAddCatDb;
+    public Button btnAddCatMovie;
+    public Button btnSaveChanges;
+    public Button btnRemoveCatDb;
+    public Button btnRemoveCatMovie;
+    public ComboBox comboBoxRemoveCatMovie;
+    public ComboBox comboBoxAddCatMovie;
+    public ComboBox comboBoxRemoveCatDb;
+}
